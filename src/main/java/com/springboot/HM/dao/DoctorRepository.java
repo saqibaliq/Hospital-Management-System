667c8e7a8a20284bfbe791cab797f3803ce4a7b8
SaqibAli <saqibq19@gmail.com>
2021-05-20 18:58:34 +0500

    Revert "HMS Completed only searching bug and url security left"
    This reverts commit e17a2b6dfbcb3d31657c628b2ba25040e67f8b55.

--- src/main/java/com/springboot/HM/dao/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.HM.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.HM.entities.User;
-
-public interface DoctorRepository extends JpaRepository<User, Long> {
-
-}
